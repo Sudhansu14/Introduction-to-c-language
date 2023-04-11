7e1cf1eea7e106dab79e122933b39541ebce3736
Sudhansu14 <130463585+Sudhansu14@users.noreply.github.com>
2023-04-11 17:26:29 +0530

    Add files via upload

---
+++ NAME.c
@@ -0,0 +1,17 @@
+#include<Stdio.h>
+main()
+{
+   printf("---------\n");
+   printf("|\t|\n");
+   printf("|\t|\n");
+   printf("|\t|\n");
+   printf("|SUDHANSU\t \n");
+   printf("|\t|\t\n");
+   printf("---------\n"); 
+   
+}
+
+
+
+
+
---
+++ dev c++ 2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	printf("--------\n");
+	printf("|\t|\n");
+	printf("S\t|\n");
+	printf("u\t|\n");
+	printf("D\t|\n");
+	printf("H\t|\n");
+	printf("A\t|\n");
+	printf("N\t|\n");
+	printf("S\t|\n");
+	printf("U\t|\n");
+	printf("|\t|\n");
+	printf("---------\n");
+}
